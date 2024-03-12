de8d0efcc39ba293ecc95c4e946ad99015a27ff7
bikramdhami0100 <bikramdhami0100@gmail.com>
2024-03-12 16:01:13 +0545

    cor

--- Arrayinphp.php
+++ Arrayinphp.php
@@ -1,18 +1,27 @@
 <?php 
 // indexed associative and multidimensional array
 //1.indexed array
-$user=["bikram",22 ,"bajhang"];
-echo $user[2]."<br/>";
+// $user=["bikram",22 ,"bajhang"];
+// echo $user[2]."<br/>";
 // for($i=0;$i<=2;$i++){
 //     echo $user[$i]."<br/>";
 // }
 // foreach ($user as $key => $value) {
 //      echo $value."<br/>";
 // }
-foreach($user as $v){
-    echo $v;
-    echo"<br/>";
-}
+// foreach($user as $v){
+//     echo $v;
+//     echo"<br/>";
+// }
 //associative array
+$arr=["name"=>"bikram dhami","age"=>22, "address"=>"bajhang"];
+foreach($arr as $key=>$data){
+    echo $key."<br/>";
+    // echo $data."<br/>";
+    // echo $key .":".$data."<br/>";
+}
+//multidimensional or 2dimensional array or neasted array
+
+
 
 ?>
