cee82ebbe90ae2607c911b5182c73607ef379d0e
bikramdhami0100 <bikramdhami0100@gmail.com>
2024-03-12 16:39:57 +0545

    yes

--- Arrayinphp.php
+++ Arrayinphp.php
@@ -16,12 +16,82 @@
 //associative array
 $arr=["name"=>"bikram dhami","age"=>22, "address"=>"bajhang"];
 foreach($arr as $key=>$data){
-    echo $key."<br/>";
+    // echo $key."<br/>";
     // echo $data."<br/>";
     // echo $key .":".$data."<br/>";
 }
 //multidimensional or 2dimensional array or neasted array
 
+$marr=[
+    [1,"bikram",22,"bajhang"],
+    [1,"bikram",22,"bajhang"],
+    [1,"bikram",22,"bajhang"],
+    [1,"bikram",22,"bajhang"]
+];
+// echo count($marr);
+// echo "<pre>";
+// print_r($marr);
+// echo "</pre>";
+// echo"<table border=1> ";
+// for ($i=0; $i <count($marr) ; $i++) { 
+//     // print_r($marr[$i]);
+//     echo"<tr>";
+//     for ($j=0; $j <count($marr[$i]) ; $j++) { 
+//         echo"<td>";
+//        echo $marr[$i][$j];
+//        echo"</td>";
+//     }
+//     echo"</tr>";
+// }
+// echo "</table>";
 
+//multidimensional associtative array
+// $users=[
+//     ["name"=>"bikram","age"=>22,"address"=>"bajhang"],
+//     ["name"=>"bikram","age"=>22,"address"=>"bajhang"],
+//     ["name"=>"bikram","age"=>22,"address"=>"bajhang"],
+//     ["name"=>"bikram","age"=>22,"address"=>"bajhang"]
+// ];
+// echo "<table border=1>";
+// foreach($users as $key=>$val){
+//     echo "<tr>";
+//     echo"<th>";
+//     foreach($val as $key1 =>$data){
+        
+//          echo $key1;
+         
+//         echo "<td>";
+//         echo $data;
+//         echo "</td>";
+        
+//     }
+//     echo"</th>";
+//     echo "</tr>";
+// }
+// echo "</table>";
+$users = [
+    ["name" => "bikram", "age" => 22, "address" => "bajhang"],
+    ["name" => "bikram", "age" => 22, "address" => "bajhang"],
+    ["name" => "bikram", "age" => 22, "address" => "bajhang"],
+    ["name" => "bikram", "age" => 22, "address" => "bajhang"]
+];
 
+// Display associative array data in a table
+echo "<table border=1>";
+// Table headers
+echo "<tr>";
+foreach ($users[0] as $key => $value) {
+    echo "<th>$key</th>";
+}
+echo "</tr>";
+
+// Table data
+foreach ($users as $user) {
+    echo "<tr>";
+    foreach ($user as $data) {
+        echo "<td>$data</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
 ?>
