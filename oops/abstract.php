35320c2a14f5acf8cdcb321110f55992c894214c
bikramdhami0100 <bikramdhami0100@gmail.com>
2024-03-29 15:43:37 +0545

    push

---
+++ oops/abstract.php
@@ -0,0 +1,25 @@
+<?php 
+abstract class Afirst{
+    abstract function productDetail();
+    abstract function ProductImage();
+    function Aone() {
+        echo "this is aone funtion";
+    }
+}
+class Asecond extends Afirst{
+    function productDetail(){
+        echo "this is product";
+    }
+    function ProductImage()  {
+         echo "this is product image";
+    }
+}
+
+// $upload=new Ofirst(); //it's not possible
+$upload =new Asecond();
+$upload->productDetail();
+echo"<br/>";
+$upload->ProductImage();
+echo"<br/>";
+$upload->Aone();
+?>
