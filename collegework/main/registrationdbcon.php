97daf4715c424b49c5a36983783289e132212226
bikramdhami0100 <bikramdhami0100@gmail.com>
2024-04-21 07:56:06 +0545

    push

--- collegework/main/registrationdbcon.php
+++ collegework/main/registrationdbcon.php
@@ -3,11 +3,7 @@ $host="localhost";
 $user="root";
 $password="";
 $connection=new mysqli($host,$user,$password);
-// if ($connection) {
-//     echo "<br/>";
-// }else{
-//     echo"connection fail";
-// }
+
 $dbname="create database if not exists students";
 $connection->query($dbname);
 // for selection of database code 
