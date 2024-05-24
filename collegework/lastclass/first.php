e1c79e7b4673d0700b59adbcb09ef6b768a3d2d9
bikramdhami0100 <bikramdhami0100@gmail.com>
2024-05-24 10:20:27 +0545

    push

---
+++ collegework/lastclass/first.php
@@ -0,0 +1,29 @@
+<?php
+ class Rarr{
+   private $name;
+   private $age;
+   private $address;
+    function __construct($n,$a,$A){
+        $this->name=$n;
+        $this->age=$a;
+        $this->address=$A;
+      
+    }
+    public function showarr() 
+     {
+        
+        $arr=array("name"=>$this->name,"age"=>$this->age,"address"=> $this->address);
+        return $arr;
+     }
+ }
+ $obj=new Rarr("bikram",22,"ktm");
+//  print_r($obj->showarr());
+$narr=$obj->showarr();
+// var_dump($narr)
+$jsonnarr=json_encode($narr);
+$jsondecodearr=json_decode($jsonnarr);
+foreach ($narr as $key => $value) {
+    echo "key is : $key and value is : $value \n <br/>";
+
+}
+?>
